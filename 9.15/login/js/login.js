b033009a2f57ba61a6ab1a470e09479b82724543
Smoke <peiyy1@lenovo.com>
2017-09-22 00:35:58 +0800

    添加键盘Enter事件

--- 9.15/login/js/login.js
+++ 9.15/login/js/login.js
@@ -1,12 +1,26 @@
-let user=document.getElementById('user');
-let pass=document.getElementById('pass');
-let button=document.getElementById('button');
-button.onclick=function(){
-	u=user.value;
-	p=pass.value;
-	if(u=="zhangsan" && p==12345){
-		alert('success')
-	}else{
-		location.replace('error.html')
+window.onload = function(){
+	let user=document.getElementById('user');
+	let pass=document.getElementById('pass');
+	let button=document.getElementById('button');
+
+	button.onclick = function(){
+		login();
+	};
+	
+	document.onkeydown = function(e){ 
+	  var ev = document.all ? window.event : e;
+	  if(ev.keyCode==13) {
+	    login();
+	  }
+	}
+	
+	function login (){
+		u=user.value;
+		p=pass.value;
+		if(u=="zhangsan" && p==12345){
+			alert('success')
+		}else{
+			location.replace('error.html')
+		}
 	}
 }
