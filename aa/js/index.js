8f613c3174259f5561417ea1c584f62caf7701ab
sunshine923 <809935627@qq.com>
2017-12-12 09:12:01 +0800

    data

--- aa/js/index.js
+++ aa/js/index.js
@@ -3,21 +3,23 @@ $(function () {
     let b = $('.b');
     let c = $('.c');
     let circle = $("div[class*='circle']")
-    a.animate({bottom:90},1000)
-    b.animate({bottom:40},1000).delay(60)
-    c.animate({bottom:-30},1000).delay(120)
-    $('.title').animate({bottom:290},1000).delay(130)
-    $('.star1').animate({bottom:-20},1000).delay(130)
-    $('.star2').animate({bottom:-40},1200).delay(130)
-    $('.star3').animate({bottom:-60},1400).delay(130)
+    a.animate({bottom:2.2+'rem'},1000)
+    b.animate({bottom:1.4+'rem'},1000).delay(60)
+    c.animate({bottom:0.4+'rem'},1000).delay(120)
+    $('.title').animate({bottom:5.8+'rem'},1000).delay(130)
+    $('.star1').animate({bottom:-0.2+'rem'},1000).delay(130)
+    $('.star2').animate({bottom:-0.4+'rem'},1200).delay(130)
+    $('.star3').animate({bottom:-0.6+'rem'},1400).delay(130)
     circle.animate({opacity:1},2000).delay(140)
     setInterval(fn,1000)
     function fn() {
         for(let i=1;i<5;i++){
-            let rights = -Math.random()*500
-            let tops = -Math.random()*200
+            let rights = -Math.random()*4-4
+            let tops = -Math.random()*2-4
             let img = $('<img>').attr('src',`img/l${i}.png`)
-            let divs = $('<div>').css({position:'absolute',top:tops+'px',right:rights+'px'}).append(img).animate({right:700,top:700},3000).delay(200*i)
+            let divs = $('<div>').addClass('yu').css({position:'absolute',top:tops+'rem',right:rights+'rem'}).append(img).delay(200*i).animate({right:9+'rem',top:9+'rem'},3000).queue(function () {
+             $(this).remove();
+            })
             divs.appendTo($('body'))
         }
     }
