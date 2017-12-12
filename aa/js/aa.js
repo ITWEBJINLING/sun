203e1c817bafb7089cf22581e5adb52fa1cc769d
sunshine923 <809935627@qq.com>
2017-12-12 11:12:36 +0800

    data

---
+++ aa/js/aa.js
@@ -0,0 +1,28 @@
+'use strict';
+
+$(function () {
+    var a = $('.a');
+    var b = $('.b');
+    var c = $('.c');
+    var circle = $("div[class*='circle']");
+    a.animate({ bottom: 1.8 + 'rem' }, 1000);
+    b.animate({ bottom: 1 + 'rem' }, 1000).delay(60);
+    c.animate({ bottom: 0 + 'rem' }, 1000).delay(120);
+    $('.title').animate({ bottom: 5.8 + 'rem' }, 1000).delay(130);
+    $('.star1').animate({ bottom: -1.1 + 'rem' }, 1000).delay(130);
+    $('.star2').animate({ bottom: -1.3 + 'rem' }, 1200).delay(130);
+    $('.star3').animate({ bottom: -1.5 + 'rem' }, 1400).delay(130);
+    circle.animate({ opacity: 1 }, 2000).delay(140);
+    setInterval(fn, 1000);
+    function fn() {
+        for (var i = 1; i < 5; i++) {
+            var rights = -Math.random() * 4 - 4;
+            var tops = -Math.random() * 2 - 4;
+            var img = $('<img>').attr('src', 'img/l' + i + '.png');
+            var divs = $('<div>').addClass('yu').css({ position: 'absolute', top: tops + 'rem', right: rights + 'rem' }).append(img).delay(200 * i).animate({ right: 9 + 'rem', top: 9 + 'rem' }, 3000).queue(function () {
+                $(this).remove();
+            });
+            divs.appendTo($('body'));
+        }
+    }
+});
